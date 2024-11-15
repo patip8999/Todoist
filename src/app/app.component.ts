e406fe77c981322e2d21e558c79c31351425d9d8
your_name <patip8999@gmail.com>
2024-11-15 21:01:18 +0100

    navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,  CommonModule],
+  imports: [RouterOutlet,  CommonModule, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
